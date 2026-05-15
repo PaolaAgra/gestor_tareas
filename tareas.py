521f2d1dfa4db9a96f7330c0f6eb5f04f53f71ba
PaolaAgra <paoa.mendoza@gmail.com>
2026-05-15 09:56:19 +0200

    segundo_commit
    Añadimos una nueva función en el documentos tareas.py

--- tareas.py
+++ tareas.py
@@ -1,3 +1,8 @@
 def agregar_tarea(lista, tarea):
-lista.append(tarea)
-return lista
+    lista.append(tarea)
+    return lista
+
+def listar_tareas(listar):
+    tareas = []
+    for i in listar:
+        tareas.append(i)
